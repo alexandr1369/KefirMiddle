2f32ee4fb9d7aad20238168acaf7fc8469792244
Alexandr <alexandr13568@gmail.com>
2022-12-29 01:01:01 +0300

    UnitModel logic has been fully tested

--- Assets/Editor/Tests/UnitTests.cs
+++ Assets/Editor/Tests/UnitTests.cs
@@ -16,16 +16,34 @@ namespace Editor.Tests
             unit.Health.Should().Be(0);
         }
         [Test]
-        public void WhenUnitHasHealth_5_AndTakingDamage_2_ThenHealthShouldBe_3()
+        public void WhenUnitHasHealth_5_AndTakingDamage_2_ThenHealthShouldBe_3_AndTookDamageEventCalled()
         {
             // Arrange
+            var wasTookDamageEventCalled = false;
             IUnit unit = new UnitModel(5);
 
             // Act
+            unit.OnTookDamage += _ => wasTookDamageEventCalled = true;
             unit.TakeDamage(2);
 
             // Assert
             unit.Health.Should().Be(3);
+            wasTookDamageEventCalled.Should().Be(true);
+        }
+
+        [Test]
+        public void WhenUnitHasHealth_10_AndTaking12Damage_ThenUnitShouldBeDestroyed()
+        {
+            // Arrange
+            var wasDestroyedEventCalled = false;
+            IUnit unit = new UnitModel(5);
+
+            // Act
+            unit.OnDestroyed += () => wasDestroyedEventCalled = true;
+            unit.TakeDamage(12);
+
+            // Assert
+            wasDestroyedEventCalled.Should().Be(true);
         }
     }
 }
